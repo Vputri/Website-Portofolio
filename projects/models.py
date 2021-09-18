b7c33e9370a5b3c3b1b619c2e02344f247409581
Vika Putri Ariyanti <vikaputriariyanti@gmail.com>
2021-09-18 19:32:32 +0700

    revisi

--- projects/models.py
+++ projects/models.py
@@ -1,10 +1,8 @@
-# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.db import models
+from ckeditor.fields import RichTextField
 
 class Project(models.Model):
     title = models.CharField(max_length=100)
-    description = models.TextField()
-    technology = models.CharField(max_length=20)
-    image = models.FilePathField(path="/img")
+    description = RichTextField()
+    image = models.ImageField(null=True, blank=True, upload_to="img/")
--- projects/static/scripts.js
+++ projects/static/scripts.js
@@ -1,4 +1,4 @@
 let x = document.querySelector(".aside");
 function myFunc() {
-    x.classList.toggle("mystyle")
+    x.classList.toggle("mystyle");
 }
