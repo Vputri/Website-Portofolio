a37106d8428a5542e7c1164fc996bd0f56303049
Vika Putri Ariyanti <vikaputriariyanti@gmail.com>
2021-10-07 15:42:41 +0700

    revisi

--- projects/static/scripts.js
+++
@@ -1,9 +0,0 @@
-let x = document.querySelector(".aside");
-function myFunc() {
-<<<<<<< HEAD
-    x.classList.toggle("mystyle");
-}
-=======
-	x.classList.toggle("mystyle")
-}
->>>>>>> 103388685c6fa346521c7a7f3a0f8b2b957a1396
